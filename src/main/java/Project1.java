6dae18d5c600354b89d00408655cd5e9fb98bdba
Ubuntu <ubuntu@ip-172-31-38-42.ec2.internal>
2020-02-19 09:37:38 +0000

    New project

---
+++ src/main/java/Project1.java
@@ -0,0 +1,11 @@
+package Project1;
+
+public class Project1 {
+
+        public static void main(String[] args){
+
+                System.out.println("Welcome to project1, Maven");
+
+        }
+
+}
